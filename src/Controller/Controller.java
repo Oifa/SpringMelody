ea25cf5ee59fdbe00858f8c4c7be7e48c4cb065a
Oifa <linfli@yandex.com>
2017-06-07 20:53:41 +1000

    Initial commit

---
+++ src/Controller/Controller.java
@@ -0,0 +1,281 @@
+package Controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable {
+
+    @FXML
+    private JFXButton btnStop, btnPlay, btnPause, btnOpen, btnBack, btnForward, btnReload, btnPrev, btnNext;
+
+    @FXML
+    private Label currentTime, totalDuration;
+
+    @FXML
+    private Slider stateSlider, volumeSlider;
+
+    @FXML
+    private AnchorPane rootPane;
+
+    @FXML
+    private ListView<String> listOfSong;
+
+    private static final double MIN_CHANGE = 0.5;
+
+
+    File file = new File("B:\\Programs\\Java\\SpringMelody\\src\\Model\\vacation-1.mp3");
+    Media media = new Media(file.toURI().toString());
+    MediaPlayer mediaPlayer = new MediaPlayer(media);
+
+
+    @FXML
+    private void LoadMusic(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("View/music.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Music");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Cant load new window");
+        }
+    }
+
+    @FXML
+    private void LoadVideo(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("View/video.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Video");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Cant load new window");
+        }
+    }
+
+    @FXML
+    private void LoadSlide(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("View/slide.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Slide");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Cant load new window");
+        }
+    }
+
+    @FXML
+    private void LoadText(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("View/text.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Text");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Cant load new window");
+        }
+    }
+
+    @FXML
+    private void LoadDoc(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("View/document.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Document");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Cant load new window");
+        }
+    }
+
+
+    @FXML
+    private void LoadAnsware(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("View/answare.fxml"));
+            Parent root1 = (Parent) fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Ask");
+            stage.setScene(new Scene(root1));
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("Cant load new window");
+        }
+    }
+
+    @FXML
+    public void Play() {
+        btnPlay.setOnAction((ActionEvent event) -> {
+            System.out.println("Playing...");
+            mediaPlayer.play();
+
+            /*  This code is used for the music slider.
+            *   Code include: value of the music state, changing state, current time,
+            *   total duration.
+            */
+            stateSlider.valueChangingProperty().addListener((obs, wasChanging, isChanging) -> {
+                if (! isChanging) {
+                    mediaPlayer.seek(Duration.seconds(stateSlider.getValue()));
+                }
+            });
+
+            stateSlider.valueProperty().addListener((obs, oldValue, newValue) -> {
+                if (! stateSlider.isValueChanging()) {
+                    double currentTime = mediaPlayer.getCurrentTime().toSeconds();
+                    if (Math.abs(currentTime - newValue.doubleValue()) > MIN_CHANGE) {
+                        mediaPlayer.seek(Duration.seconds(newValue.doubleValue()));
+                    }
+                }
+            });
+
+
+            mediaPlayer.currentTimeProperty().addListener((observable, oldTime, newTime) -> {
+                if (! stateSlider.isValueChanging()) {
+                    stateSlider.setValue(newTime.toSeconds());
+                }
+            });
+
+            stateSlider.valueProperty().addListener(new ChangeListener<Number>() {
+                public void changed(ObservableValue<? extends Number> ov, Number oldValue, Number newValue) {
+                    currentTime.setText(String.format("%02d", newValue.intValue()));
+                    totalDuration.setText(String.format("%.2f", (volumeSlider.getValue() / 180)*6));
+                }
+            });
+
+            /*  This code changing volume of music.
+            *   Min state = 0; max state = 100
+            */
+            volumeSlider.setValue(mediaPlayer.getVolume() * 100);
+            volumeSlider.valueProperty().addListener(new InvalidationListener() {
+                @Override
+                public void invalidated(Observable observable) {
+                    mediaPlayer.setVolume(volumeSlider.getValue() / 100);
+                }
+            });
+
+        });
+    }
+
+
+
+
+    @FXML
+    public void Pause() {
+        btnPause.setOnAction((ActionEvent e) -> {
+            System.out.println("PAUSE");
+            mediaPlayer.pause();
+        });
+    }
+
+    @FXML
+    public void Stop() {
+        btnStop.setOnAction((ActionEvent e) -> {
+            System.out.println("stop");
+            mediaPlayer.stop();
+        });
+    }
+
+    @FXML
+    public void Prev() {
+        btnPrev.setOnAction((ActionEvent e) -> {
+            System.out.println("prev");
+            mediaPlayer.seek(mediaPlayer.getStartTime());
+            mediaPlayer.stop();
+        });
+    }
+
+    @FXML
+    public void Next() {
+        btnNext.setOnAction((ActionEvent e) -> {
+            System.out.println("next");
+            mediaPlayer.seek(mediaPlayer.getTotalDuration());
+            mediaPlayer.stop();
+        });
+    }
+
+    @FXML
+    public void Forward() {
+        btnForward.setOnAction((ActionEvent e) -> {
+            mediaPlayer.seek(mediaPlayer.getCurrentTime().multiply(1.5));
+        });
+    }
+
+    @FXML
+    public void Back() {
+        btnBack.setOnAction((ActionEvent e) -> {
+            mediaPlayer.seek(mediaPlayer.getCurrentTime().divide(1.5));
+        });
+    }
+
+    @FXML
+    public void Reload() {
+        btnReload.setOnAction((ActionEvent e) -> {
+            System.out.println("reload");
+            mediaPlayer.seek(mediaPlayer.getStartTime());
+        });
+    }
+
+
+
+    @FXML
+    public void Open() {
+        btnOpen.setOnAction((ActionEvent e) -> {
+            try {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.getExtensionFilters().add(new ExtensionFilter("*.mp3", "All files", "*.*"));
+                File file = fileChooser.showOpenDialog(null);
+                String path = file.getAbsolutePath();
+                path = path.replace("\\", "/");
+                media = new Media(new File(path).toURI().toString());
+                mediaPlayer.stop();
+                mediaPlayer = new MediaPlayer(media);
+            } catch (Exception e1) {
+                System.out.println("Exit");
+            }
+        });
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
+
+
+}
